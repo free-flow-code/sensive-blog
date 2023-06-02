783cad66675d04e62a5d3f56731fa648244d78b0
free-flow-code <wildmuskaria8@gmail.com>
2023-06-02 17:56:25 +0400

    Reduced number of queries to DB.

--- blog/views.py
+++ blog/views.py
@@ -50,7 +50,7 @@ def index(request):
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
 
-    comments = post.comments.prefetch_related('author').all()
+    comments = post.comments.select_related('author').all()
     serialized_comments = []
     for comment in comments:
         serialized_comments.append({
